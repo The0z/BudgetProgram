fec904a431d8512cf527c0cb4ab0e0086ae4e937
Austin Gallagher <ozzygallagher@gmail.com>
2020-09-07 12:16:56 -0600

    Passing 9 of 11 unit tests. Updated __str__ function to print summary

--- budget.py
+++ budget.py
@@ -5,17 +5,54 @@ class Category:
         self.balance = 0
         self.ledger = list()
 
-    # def create_spend_chart(categories):
-    #    print("Hello World")
+    def __str__(self):
+        MAX_LEN = 30
+        MAX_DSC = 23
+        MAX_AMT = 7
+        dsc = ""
+
+        title = self.titleMaker(MAX_LEN)
+        for i in range(len(self.ledger)):
+            dsc = dsc + "\n" + self.descriptionMaker(MAX_DSC, MAX_AMT, i)
+        return title + dsc
+
+    def titleMaker(self, MAX_LEN):
+        titleSize = len(self.categoryName)
+        if(titleSize > MAX_LEN):
+            title = self.categoryName[:MAX_LEN]
+        else:
+            stars = int((MAX_LEN - titleSize)/2)
+            title = "*"*stars + self.categoryName + "*"*stars
+            if(len(title) < MAX_LEN):
+                title = title+"*"
+        return title
+
+    def descriptionMaker(self, MAX_DSC, MAX_AMT, i):
+        dscSize = len(self.ledger[i].get("description"))
+        if(dscSize > MAX_DSC):
+            dsc = self.ledger[i].get("description")[:MAX_DSC]
+        else:
+            space = int(MAX_DSC - dscSize)
+            dsc = self.ledger[i].get("description") + " "*space
+
+        amtFormat = ("{:"+str(MAX_AMT)+".2f}").format(self.ledger[i].get(
+                                                    "amount"))
+        numSize = len(amtFormat)
+        if(numSize > MAX_AMT):
+            amt = amtFormat[0:MAX_AMT]
+        else:
+            space = int(MAX_AMT - numSize)
+            amt = " "*space + amtFormat
+        return dsc + amt
 
     def deposit(self, amount, description=""):
         self.balance = amount
         self.ledger.append({"amount": amount, "description": description})
 
-    def withdraw(self, amount, description="withdrawal"):
+    def withdraw(self, amount, description=""):
         # locate the item in the ledger
         for i in range(len(self.ledger)):
-            if self.balance >= amount:
+            if self.check_funds(amount):
                 self.ledger.append({"amount": -1*amount, "description":
                                     description})
                 self.balance = self.balance - amount
@@ -28,14 +65,22 @@ class Category:
 
     def transfer(self, amount, category):
         # Enough funds Transfer return true
-        dsc = "Transfer to " + category.categoryName
-        if self.withdraw(amount, dsc):
-            depDsc = "Transfer From " + self.categoryName
+        if self.check_funds(amount):
+            dsc = "Transfer to " + category.categoryName
+            self.withdraw(amount, dsc)
+            depDsc = "Transfer from " + self.categoryName
             category.deposit(amount, depDsc)
             return True
         # Not enough funds do not transfer return false
         else:
             return False
 
-    def check_funds():
-        return 0
+    def check_funds(self, amount):
+        if self.balance >= amount:
+            return True
+        else:
+            return False
+
+
+def create_spend_chart(categories):
+    print("Hello World")
--- main.py
+++ main.py
@@ -8,24 +8,18 @@ food.deposit(1000, "initial deposit")
 food.withdraw(10.15, "groceries")
 food.withdraw(15.89, "restaurant and more food for dessert")
 print(food.get_balance())
-
 clothing = budget.Category("Clothing")
 food.transfer(50, clothing)
-print(food.get_balance())
 clothing.withdraw(25.55)
 clothing.withdraw(100)
-print(clothing.get_balance())
+auto = budget.Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
 
-print(food.ledger)
-print(clothing.ledger)
-# auto = budget.Category("Auto")
-# auto.deposit(1000, "initial deposit")
-# auto.withdraw(15)
-#
-# print(food)
-# print(clothing)
-#
+print(food)
+print(clothing)
+
 # print(create_spend_chart([food, clothing, auto]))
 
 # Run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
